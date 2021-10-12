a27d585eb7b3c125743a86a87edf8fe100582a98
Eric R <eric92rodriguez@gmail.com>
2021-10-12 19:22:24 -0300

    2do tp unidad 2

--- src/description.js
+++ src/description.js
@@ -11,6 +11,20 @@ function Description() {
   if (boolean === true)  {
     gracias = "Gracias por su comprar"
   }
+
+  var [det, setDet] = useState(0);
+  let detalleTitulo = ""
+  let detalle = ""
+  let detalle2 = ""
+  if (det === true)  {
+    detalleTitulo = "Anteojos de sol Lentes Infinit Moscow"
+    detalle ="Las lentes de sol INFINIT de alta protección están pensadas para realizar actividades al aire libre. Los cristales lentes de sol con filtro UV400 son capaces de bloquear el 100% los rayos solares UV400A y UV400B. Nuestro material orgánico de Policarbonato es de altísima resistencia a los impactos evitando roturas o accidentes oculares."
+    detalle2="Los colores Mate pueden tener pequeños brillados que se ocasionan por la propia manipulación entre las piezas de producción. Esta superficie mate sutilmente aterciopelada debe tratarse con cuidado para evitar los brillados en la superficie. Los mismos no constituyen un defecto de fabricación."
+  }else{
+    detalleTitulo = ""
+    detalle = ""
+    detalle2 = ""
+  }
   return (
     <div classDescription="App">
       <header classDescription="App-header">
@@ -21,15 +35,11 @@ function Description() {
 
 
        <div class="card bg-dark mb-3" >
+       <button type="button" class="btn btn-primary" onClick={() => setDet(det = true || false )}>Detalle</button>
           <div class="card-body">
-            <h5 class="card-title">Anteojos de sol Lentes Infinit Moscow</h5>
-            <h3 class="card-text">Las lentes de sol INFINIT de alta protección están pensadas para realizar actividades al aire libre.
-
-              Los cristales lentes de sol con filtro UV400 son capaces de bloquear el 100% los rayos solares UV400A y UV400B.
-
-              Nuestro material orgánico de Policarbonato es de altísima resistencia a los impactos evitando roturas o accidentes oculares.</h3>
-              <h6>
-              Los colores Mate pueden tener pequeños brillados que se ocasionan por la propia manipulación entre las piezas de producción. Esta superficie mate sutilmente aterciopelada debe tratarse con cuidado para evitar los brillados en la superficie. Los mismos no constituyen un defecto de fabricación.
+            <h5 class="card-title">{detalleTitulo}</h5>
+            <h3 class="card-text">{detalle}</h3>
+              <h6>{detalle2}
               </h6>
               <h5> Medidas: </h5>
               <ol class="list-group list-group-numbered">
